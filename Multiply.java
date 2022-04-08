af72d6547c4bb6f96d39e0c018308f9e2dfb8917
Imran Ali <imranali.code@gmail.com>
2022-04-08 14:29:00 +0530

    added multiply.java with Sum.java

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+        System.out.println("The Product of a and b is " + (a * b));
+    }
+
+}
